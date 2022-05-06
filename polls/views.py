3f4b0c23ef63c071bfd84ce3d11979ec730b19de
João Antônio Barbosa <joaobarbosadev@gmail.com>
2022-05-05 21:32:46 -0300

    Added new url-mapped views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse('Hello, world. You are at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:3]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
